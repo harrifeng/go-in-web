0ec8883d76d1dded175b7b1cac64140c66ecff3c
Harri Feng <harrifeng@gmail.com>
2017-10-06 14:03:23 +0000

    first workable

---
+++ 01-bbs/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+	mux.HandleFunc("/", index)
+	server := &http.Server{
+		Addr:    "0.0.0.0:7890",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+
+	fmt.Println()
+	os.Exit(0)
+}
+
+func index(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(writer, "Hello World, %s", request.URL.Path[1:])
+}
